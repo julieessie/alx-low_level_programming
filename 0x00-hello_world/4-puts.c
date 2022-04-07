20b86007bd6c1064e18b875be1c12847318b2aba
julieessie <julienyambura50@gmail.com>
2022-04-07 08:12:31 -0700

    write a C program that prints a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/*main function {puts function to print a string instead of printf}
+return value 0
+*/
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
