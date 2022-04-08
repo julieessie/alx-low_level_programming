22df95b0cfbf2492a9002ce64d600005886f14c7
julieessie <julienyambura50@gmail.com>
2022-04-08 13:23:15 -0700

    task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+
+{
+char lowercase;
+char uppercase;
+
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++);
+{
+putchar(lowercase);
+}
+
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++);
+{
+putchar(uppercase);
+}
+
+putchar('\n');
+
+return (0);
+
+}
