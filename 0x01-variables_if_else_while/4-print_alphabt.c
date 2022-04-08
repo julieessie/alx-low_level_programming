a9183562554775cf65aff27ffbaa1208a6a93952
julieessie <julienyambura50@gmail.com>
2022-04-08 14:10:20 -0700

    Task-4 Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Empty point
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet<= 'z'; alphabet++)
+{
+ 
+if (alphabet == 'q' | 'e')
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
