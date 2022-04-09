c3e374a6986c114a177defd05e80112e21fc4afc
julieessie <julienyambura50@gmail.com>
2022-04-08 18:53:18 -0700

    Task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry point 
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+
+int ncomb;
+
+for (ncomb = 48; ncomb <= 57; ncomb++)
+{
+putchar(ncomb);
+
+if (ncomb == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+
+return(0);
+}
