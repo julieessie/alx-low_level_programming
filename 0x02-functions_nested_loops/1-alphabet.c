f245bf75ad2b30502c922e86d02e608aa3912bc1
julieessie <julienyambura50@gmail.com>
2022-04-12 06:11:39 -0700

    Task-1 Alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * prints alphabet in lowercase
+ */
+void print_alphabet(void);
+{
+char alphabets;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+_putchar(alphabets);
+_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H
 
 int _putchar(char c);
+void print_alphabet(void);
 #endif /* _MAIN_H */
