176b1a6d124107b2b96083d6e3b05f15b48efe3f
julieessie <julienyambura50@gmail.com>
2022-04-13 11:24:03 -0700

    Task-11  prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
-#include "main.h"
+#include "stdio.h"
 /**
- * print_to_98 -  prints all natural numbers to ninety eight
+ *print_to_98 -  prints all natural numbers to ninety eight
  *@n: number to be counted from.
  */
 void print_to_98(int n)
