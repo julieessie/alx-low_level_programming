f2b5ebca04e72897958c236a13e7cd1a501a322d
julieessie <julienyambura50@gmail.com>
2022-04-13 00:38:41 -0700

    Task-2  prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 int count = 0;
 char alphabets;
 
-while (count++ <+9)
+while (count++ <= 9)
 {
 for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 _putchar(alphabets);
