1a14e5560d3fe0c25ce7ea1673a55322cd5488f8
julieessie <julienyambura50@gmail.com>
2022-04-14 10:56:39 -0700

    Task-3 prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_numbers - prints the numbers from zero to nine
  *
- * Return: numbers
+ * Return: Always 0.
  */
 
 void print_numbers(void)
@@ -10,10 +10,10 @@ void print_numbers(void)
   
 int i;
  
-for (i = 0; i <= 10; i++)
-
+for (i = 0; i < 10; i++)
+{
 _putchar(i + '0');
-
+}
 
 _putchar('\n');
 }
