0f90096865748ea588a9b137d30dcaa6c287466c
julieessie <julienyambura50@gmail.com>
2022-04-14 11:38:35 -0700

    Task-4 t prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ * Return: Always 0;
+ */
+
+void more_numbers(void)
+
+{
+int i;
+int num1;
+int num2;
+int result;
+
+i = 0;
+result = 0;
+
+while (i < 10)
+{
+while (result <= 14)
+{
+if (result < 10)
+{
+num2 = result;
+}
+else
+{
+num1 = result / 10;
+num2 = result % 10;
+}
+_putchar(num2 + '0');
+result++;
+}
+
+i++;
+result = 0;
+_putchar('\n');
+}
+}
