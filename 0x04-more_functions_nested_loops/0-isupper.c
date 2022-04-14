642df291c0487e4ff04749cc1902df5ef56e6492
julieessie <julienyambura50@gmail.com>
2022-04-14 03:30:59 -0700

    Task-0 checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * isupper - checks for uppercase character
+ * @c: to print int
+ * Return: 1 if uppercase or 0 if otherwise
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c<= 'Z')
+{
+
+return (1);
+
+}
+else
+{
+return (0);
+}
+}
