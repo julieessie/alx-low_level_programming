130df742dbfb7eaa586ed4f595c0e8a5fd1308a9
julieessie <julienyambura50@gmail.com>
2022-04-17 01:16:35 -0700

    Task-3 Number speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,6 +7,8 @@ void print_numbers(void)
 {
 int i;
 
+i = 0;
+
 for (i = 0; i < 10; i++)
 
 putchar(i + '0');
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
  * print_most_numbers - print numbers 
  * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
 int i;
