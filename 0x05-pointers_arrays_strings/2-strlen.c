d72c4a0704855012979ee60d2c782a4a23264869
julieessie <julienyambura50@gmail.com>
2022-04-20 05:51:45 -0700

    by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to an int that returns length
+ * Return: Always 0;
+ */
+int _strlen(char *s)
+{
+int l;
+
+for (l = 0; s[l] != '\0'; l++)
+
+{
+continue;
+}
+return (l);
+  
