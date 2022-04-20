28250b3ec2cf03f728b0345d6a7529c7ce595423
julieessie <julienyambura50@gmail.com>
2022-04-20 09:19:38 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 -  prints every other character of a string
+ * @str: pointer to an int where changes are being made
+ * Return: void
+ */
+void puts2(char *str);
+{
+int ch;
+
+for (ch = 0; str[ch] != '\0'; ch++)
+{
+if (ch % 2 == 0)
+	
+_putchar(str[ch]);
+}
+_putchar('\n');
+}
