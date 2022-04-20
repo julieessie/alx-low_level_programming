27edc09650d53d4ad256f6189ddc04f216763c81
julieessie <julienyambura50@gmail.com>
2022-04-20 07:13:46 -0700

    I do not fear computers, I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: pointer to an integer that prints string
+ * Return: void
+ */
+void _puts(char *str)
+{
+char *c;
+int v;
+
+c = str;
+
+for (v = 0; c[v] v++)
+{
+_putchar(c[v]);
+}
+_putchar('\n');
+}
+
+_
