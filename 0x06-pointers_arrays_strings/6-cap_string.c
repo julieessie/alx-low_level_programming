e1d3cafb546b82001a44d0b15cd90478b7a0d670
julieessie <julienyambura50@gmail.com>
2022-04-24 13:55:20 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: string
- * Return: a pointer
+ * Return: s
  */
 char *cap_string(char *)
 {
@@ -19,7 +19,7 @@ for (i = 0; i < 13; i++)
 if (*(s + k) == separators[i])
 {
 if ((*(s + (k + 1)) >= 97) && (*(s + (k + 1)) <= 122))
-  *(s + (k + 1)) = *(s + (k + 1)) -32;
+  *(s + (k + 1)) = *(s + (k + 1)) - 32;
 break;
 }
 }
