258ed954c067c08b669213ad12fb793cae720628
julieessie <julienyambura50@gmail.com>
2022-04-23 18:38:45 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: a pointer to a char that will be updated
+ * @src: a pointer to a char that will be updated
+ * @n: value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, k;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+
+k = 0;
+while (k < n && src[k] != '\0')
+{
+dest[i] = src[k];
+k++;
+i++;
+}
+
+dest[i] = '\0';
+
+return (dest);
+
+}
+   
