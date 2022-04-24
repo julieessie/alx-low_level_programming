e90f7ca941c96a7d0ae6d3abe1a4e2baa109e2af
julieessie <julienyambura50@gmail.com>
2022-04-24 14:11:53 -0700

    Mozart composed his music

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet -  encodes a string into 1337
+ * @s: input string
+ * Return: the pointer dest
+ */
+{
+int k = 0;
+int = i;
+int lowercase[] = {97, 101, 111, 116, 108};
+int uppercase[] = (65, 69, 79, 84, 76);
+int numbers[] = {52, 51, 48, 55, 49);
+
+while (*(s + k) != '\0')
+{
+for (i = 0; i < 5; i++)
+{
+if (*(s + k) == lowercase[i] || *(s + k) == uppercase[i])
+{
+*(s + k) = numbers[i];
+break;
+}
+}
+k++;
+}
+return (s);
+}
