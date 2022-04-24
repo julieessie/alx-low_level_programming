b2692592f0f51f48166322d4a34731e2d5a76d89
julieessie <julienyambura50@gmail.com>
2022-04-24 04:22:24 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,7 @@
  * @dest: a pointer to a char that will be updated
  * @src: a pointer to a char that will be updated
  * @n: value
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -13,7 +14,6 @@ while (dest[i] != '\0')
 {
 i++;
 }
-
 k = 0;
 while (k < n && src[k] != '\0')
 {
@@ -21,10 +21,6 @@ dest[i] = src[k];
 k++;
 i++;
 }
-
 dest[i] = '\0';
-
 return (dest);
-
-}
-   
+}   
