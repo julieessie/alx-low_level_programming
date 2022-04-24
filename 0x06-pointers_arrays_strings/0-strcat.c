94b46e66e5590095a44ce6672182df3912325312
julieessie <julienyambura50@gmail.com>
2022-04-23 18:15:18 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,13 @@
 /**
  * _strcat - concantenates two strings
  * @dest: a pointer to a character that will be updated
- * @src:  a pointer to a character that will be updated
- * Return:  dest
+ * @src: a pointer to a character that will be updated
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 int i;
 int k;
-
 i = 0;
 while (dest[i] != '\0')
 {
@@ -24,5 +23,4 @@ i++;
 }
 dest[i] = '\0';
 return (dest);
-}
-  
+}  
