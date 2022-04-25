de28f215fe069413ba5d4da82ac650da66587b49
julieessie <julienyambura50@gmail.com>
2022-04-25 07:53:23 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-r0t13.c
+++ 0x06-pointers_arrays_strings/100-r0t13.c
@@ -8,14 +8,13 @@ char *rot13(char *s)
 {
 int count = 0;
 int i;
-char alphabets[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 while (*(s + count) != '\0')
 {
-
+  
 for (i = 0; i < 52; i++)
-	
 {
 	  
 if (*(s + count) == alphabet[i])
