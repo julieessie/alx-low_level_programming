c7b05f29c8d9d9a2c6862ad55f4a819397e6e7a5
julieessie <julienyambura50@gmail.com>
2022-04-25 12:54:28 -0700

    A dream doesnt become reality through magic

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,7 +6,7 @@
  */
 char *rot13(char *s)
 {
-  int count = 0, i;
+int count = 0, i;
 char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char *rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
