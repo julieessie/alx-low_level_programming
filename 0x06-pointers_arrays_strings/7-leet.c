7e39b6859ff164050d834040fa0ce1e0b046a936
julieessie <julienyambura50@gmail.com>
2022-04-25 07:32:31 -0700

    rot13

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * leet - encodes a string
+ * leet - encodes a string into 1337
  * @s: input string
  * Return: s 
  */
@@ -10,8 +10,8 @@ int k = 0;
 int i;
 int lowercase[] = {97, 101, 111, 116, 108};
 int uppercase[] = {65, 69, 79, 84, 76};
-int numbers[] = {52, 51, 48, 55, 49);
-
+int numbers[] = {52, 51, 48, 55, 49};
+  
 while (*(s + k) != '\0')
 {
 for (i = 0; i < 5; i++)
