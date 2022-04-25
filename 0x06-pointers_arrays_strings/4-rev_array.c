e4259f22788b289be285b75bd963179eed4d1832
julieessie <julienyambura50@gmail.com>
2022-04-24 19:09:24 -0700

    I am a kind of paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-int i, j;
-for (i = 0; i < n / 2; i++)
+int i;
+int j;
+for (i = 0; i < n; i++)
 {
 j = a[i];
 a[i] = a[n - i - 1];
 a[n - i -1] = j;
 }
-return (j);
 }
