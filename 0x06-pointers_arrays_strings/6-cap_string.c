2820228c675849bd5815cebccecbe00b963508ef
julieessie <julienyambura50@gmail.com>
2022-04-24 19:42:51 -0700

    Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,13 +2,13 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: string
- * Return: char s
+ * Return: s
  */
 char *cap_string(char *s)
 {
 int k = 0;
 int = i;
-int separators [] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 if (*(s + k) >= 97 && *(s + k) <= 122)
 *(s + k) = *(s + k) - 32;
 k++;
