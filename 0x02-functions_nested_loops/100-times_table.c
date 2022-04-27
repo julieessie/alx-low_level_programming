21cb491cdaa4eca8611384eb5a88fff3c1d61969
julieessie <julienyambura50@gmail.com>
2022-04-26 18:17:31 -0700

    Task-12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,41 @@
  */
 void print_times_table(int n)
 {
-  int x, y, result;
+int x, y, result;
 
-  if(!(n > 15 || n < 0))
-    {
-      for (x = 0; x <= n; x++)
-	{
-	  for( y = 0; y <= n; y++)
-	    {
-	      result = (x * y);
-	      if (y != 0)
-		{
-		  putchar(',');
-		  putchar(' ');
-		}
-	      if (result < 10 && y != 0)
-		{
-		  putchar(' ');
-		  putchar(' ');
-		  putchar((res % 10) + '0');
-		}
-	      else if (res >= 10 && res < 100)
-		{
-		  putchar(' ');
-		  putchar((res / 10) + '0');
-		  putchar((res % 10) + '0');
-		}
-	      else if (res >= 100 && y != 0)
-		{
-
-		  putchar((res / 100) + '0');
-		  putchar((res / 10) % 10 + '0');
-		  putchar((res % 10) + '0');
-		  
-		}
-	      else
-		putchar((res % 10) + '0');
-	    }
-	  putchar('\n');
-	}
-    }
+if(!(n > 15 || n < 0))
+{for (x = 0; x <= n; x++)
+{
+for( y = 0; y <= n; y++)
+{
+result = (x * y);
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (result < 10 && y != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else if (res >= 10 && res < 100)
+{
+_putchar(' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0')
+}
+else if (res >= 100 && y != 0)
+{
+_putchar((res / 100) + '0');
+_putchar((res / 10) % 10 + '0');
+_putchar((res % 10) + '0'); 
+}
+else
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
