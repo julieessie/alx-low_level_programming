a8afbae8c26b22486d13a850d399be66a4eca42b
julieessie <julienyambura50@gmail.com>
2022-04-26 20:16:17 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,13 +12,11 @@ int i;
 int sum1, sum2;
 sum1 = 0;
 sum2 = 0;
-
 i = 0;
 while (i < size)
 {
 sum1 = sum1 + *(a + i * size + i);
 sum2 = sum2 + *(a + i * size + size - i - 1);
-
 i++;
 }
 printf("%i, %i\n", sum1, sum2);
