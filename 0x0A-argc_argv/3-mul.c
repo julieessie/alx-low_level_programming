46c0b62d20fb9fb3ee7069cbf5a6736e41fa4bf5
julieessie <julienyambura50@gmail.com>
2022-05-03 20:13:12 -0700

    Task-3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @rgc: count argument
+ * @argv: argument char
+ * Return: 0 if true 1 if false
+ */
+int main(int argc, char *argv[]){
+int k, j;
+if (argc == 3)
+{
+k = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", k *j);   
+return (0);
+}
+printf("Error\n");
+return (1);
+}
