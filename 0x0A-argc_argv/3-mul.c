cd540f0a447c298cd279a72ff245e9836d79d869
julieessie <julienyambura50@gmail.com>
2022-05-05 03:14:50 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,7 @@
  * main - multiplies two numbers
  * @argc: count argument
  * @argv: argument char
- * Return: 0 if true 1 if false
+ * Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
@@ -14,7 +14,7 @@ if (argc == 3)
 k = atoi(argv[1]);
 j = atoi(argv[2]);
 printf("%d\n", k *j);
-return (0)
+return (0);
 }
 printf("Error\n");
 return (1);
