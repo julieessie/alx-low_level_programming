fead179f529e67a8cdc5697c5454465e25bc77e9
julieessie <julienyambura50@gmail.com>
2022-05-08 14:16:27 -0700

    4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,1 +1,35 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the number of arguments.
+ * @argc: argument count
+ * @argv: argument vector, array
+ * Return: 1 if error 0 if correct
+ */
+int main(int argc, char *argv[])
+{
+int i, k;
+int sum;
+
+sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+for (k = 0; argv[i]k]; k++)
+{
+if (argv[i][k] < 48 || argv[i][k] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
+sum = strtol(argv[i]);
+}
+if (argc <= 1)
+{
+printf("0\n");
+}  
+else
+printf("%d\n", sum);
+return (0);
+}
