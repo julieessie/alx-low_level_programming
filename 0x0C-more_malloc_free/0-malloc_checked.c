734dee4a6f31e1c900475a773cac4f9c26ed10bc
julieessie <julienyambura50@gmail.com>
2022-05-10 04:19:58 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,9 +4,6 @@
  * malloc_checked - allocates memory using malloc
  * @b: argument which is an integer
  * Return: a pointer to allocated memory
- * if malloc fails, the malloc checked function
- * should cause normal process termination
- * with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
