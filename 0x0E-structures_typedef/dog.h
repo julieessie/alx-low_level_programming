76a014589dbacba41ba87f677db1551a063d1c60
julieessie <julienyambura50@gmail.com>
2022-05-12 18:44:16 -0700

    Task-3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ float age;
 char *owner;
 };
 
-typedef struct dog dog_rt;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
