73e919d5cf5623259f7c14da5cf4efe754ef5ce9
julieessie <julienyambura50@gmail.com>
2022-05-16 07:41:00 -0700

    Task -1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints a name
+ * @name: name of person to print
+ * @f: pointer to the print function
+ * Return: void 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+  if (name && f)
+    f(name);
+}
