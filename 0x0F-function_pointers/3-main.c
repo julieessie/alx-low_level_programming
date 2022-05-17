f7c146398ac9f50535754d011141c72e561a64ab
julieessie <julienyambura50@gmail.com>
2022-05-17 16:41:34 -0700

    Task-3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int n_1, n_2;
 char *op;
 
 if (argc != 4)
@@ -18,9 +18,9 @@ if (argc != 4)
 printf("Error\n");
 exit(98);
 }
-num1 = atoi(argv[1]);
+n_1 = atoi(argv[1]);
 op = argv[2];
-num2 = atoi(argv[3]);
+n_2 = atoi(argv[3]);
 
 if (get_op_func(op) == NULL || op[1] != '\0')
 {
@@ -28,13 +28,13 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+if ((*op == '/' && n_2 == 0) || (*op == '%' && n_2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
 
-printf("%d\n", get_op_func(op)(num1, num2));
+printf("%d\n", get_op_func(op)(n_1, n_2));
 
 return (0);
 }
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arg passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+int i = 0;
+char *str_p;
+char *  
