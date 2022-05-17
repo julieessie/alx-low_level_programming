10359b9c2bc809bc60e28f21cbaad440dbeddc4d
julieessie <julienyambura50@gmail.com>
2022-05-17 06:26:39 -0700

    Task-3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ printf("Error\n");
 exit(98);
 }
 num1 = atoi(argv[1]);
-op = *argv[2];
+op = argv[2];
 num2 = atoi(argv[3]);
 
 if (get_op_func(op) == NULL || op[1] != '\0')
@@ -27,7 +27,7 @@ printf("Error\n");
 exit(99);
 }
 
-if ((*op == '/' && num2 == 0 || (*op == '%') && num2 == 0))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
