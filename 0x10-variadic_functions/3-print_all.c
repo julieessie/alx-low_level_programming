000490379c374da6b2fc2cb019eee62f7f47194f
julieessie <julienyambura50@gmail.com>
2022-05-19 18:29:09 -0700

    Task-3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
 /**
  * print_all - function that prints anything.
  * @format: list of types of arg passed to the function
@@ -8,7 +10,6 @@ void print_all(const char * const format, ...)
 {
 int i = 0;
 char *str_p;
-
 va_list l;
 
 va_start(l, format);
