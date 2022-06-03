cb7bc5d75d4fa8eeb1d1e17cce591e7a7be60029
julieessie <julienyambura50@gmail.com>
2022-06-03 00:51:09 +0000

    Task-1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - creates a file
  * @filename: name of file to create
  * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
+ * Return: 1 if successful -1 if it fails
  */
 int create_file(const char *filename, char *text_content)
 {
