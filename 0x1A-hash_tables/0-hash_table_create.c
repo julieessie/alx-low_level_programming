cac781304e3980792f522a826264e5c73a3a7002
julieessie <julienyambura50@gmail.com>
2022-07-29 20:32:26 +0000

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,22 +10,14 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_p;
-unsigned long int i;
+hash_table_t *ht;
 
 if (size == 0)
 return (NULL);
-hash_p = malloc(sizeof(hash_table_t));
-if (hash_p == NULL)
+
+ht = malloc(sizeof(hash_table_t));
+if (!ht)
 return (NULL);
-hash_p->size = size;
-hash_p->array = malloc(sizeof(hash_node_t *) * size);
-if (hash_p->array == NULL)
-{
-free(hash_p);
-return (NULL);
-}
-for (i = 0; i < size; i++)
-hash_p->array[i] = NULL;
-return (hash_p);
+
+return (ht);
 }
